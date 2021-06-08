c0c13aed329c66668f1ae035a754da844ddf6f07
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-08 23:17:27 +0545

    Function Overloading

---
+++ Assignment/Assignment-3/function-overloading.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int add(int a,int b)
+{
+    cout<<"sum=:"<<a+b<<endl;
+    return 0;
+}
+double add (double a, int b, double c)
+{
+    cout<<"sum="<<a+b+c<<endl;
+    return 0;
+}
+double add (int a, double b)
+{
+    cout<<"sum="<<a+b<<endl;
+    return 0;
+}
+int main()
+{
+    add(10,20);
+    add(10.5,10,2.5);
+    add(10.5,10,2);
+    return 0;
+}
