eb3217bc801a3a1a07363834ec77f0e0525544c9
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-08 23:00:10 +0545

    Area Of Tringle

---
+++ Assignment/Assignment-2/area_of_triangle2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int area();
+int main()
+{
+int area;
+{
+float B,H,area;
+cout<<"Enter the value of Base:";
+cin>>B;
+cout<<"Enter the value of Hight:";
+cin>>H;
+area=(0.5*B*H) ;
+cout<<"Area of triangle with base and hight :\n"<<area;
+
+return 0;
+}}
