a9bfa4a4e354aa8c6d7806d1458eeb8e4b584ca5
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-12 20:04:55 +0545

    with argument and no  return value

---
+++ cpp-lab/lab-3/user-defined-function/example3.cpp
@@ -0,0 +1,19 @@
+//using with argument and no return value calc.of area of triangle
+
+#include<iostream>
+using namespace std;
+void getarea(float,float);
+int main()
+{
+    float base,height,area;
+    cout<<"input base and height:"<<endl;
+    cin>>base>>height;
+    getarea(base,height);
+      
+} 
+void getarea(float b,float h)
+{
+    float area;
+    area=((0.5)*b*h);
+    cout<<"area of triangle is:"<<area<<endl;         
+}
