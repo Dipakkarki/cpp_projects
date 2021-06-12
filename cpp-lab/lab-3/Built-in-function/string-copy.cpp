4709881e59d4ad3bb5e345736432b2d8c31b4a17
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-12 19:41:26 +0545

    use of strcpy()

---
+++ cpp-lab/lab-3/Built-in-function/string-copy.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char string[50],str[50];
+    cout<<"Input your string:"<<endl;
+    cin>>string;
+    strcpy(str,string);
+    cout<<"Copied string is:"<<str<<endl;
+}
