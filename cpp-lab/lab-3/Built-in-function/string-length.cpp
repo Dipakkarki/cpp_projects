78ee814a497875545b7f1b886269b488c7b6667f
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-12 19:42:27 +0545

    use of strlen()

---
+++ cpp-lab/lab-3/Built-in-function/string-length.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char string[50];
+    int length;
+    cout<<"Input your string:"<<endl;
+    cin>>string;
+    length=strlen(string);
+    cout<<"Length is:"<<length<<endl;
+}
