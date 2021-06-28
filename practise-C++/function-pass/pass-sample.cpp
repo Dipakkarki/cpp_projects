50b7800afcfcd1bc0ec9c69e21d73a9ded923b11
Dipakkarki <anubhabkarki66@gmail.com>
2021-06-28 22:35:36 +0545

    pass-sample.cpp

---
+++ practise-C++/function-pass/pass-sample.cpp
@@ -0,0 +1,24 @@
+//Sample program to demonstrate pass by value and pass by reference
+#include<iostream>
+using namespace std;
+void functionfirst(int a)
+{
+    cout<<"value of a before altering:"<<a<<endl;
+    a=5;
+    cout<<"value of a after altering:"<<a<<endl;
+
+}
+void functionReference (int &b)
+{
+    b=20;
+    cout<<"value of b after altering:"<<b<<endl;
+}
+int main()
+{
+    int a=1;
+    int b=2;
+    functionfirst(a);
+    functionReference(b);
+    cout<<"value of a in main:"<<a<<endl;
+    cout<<"value of b in main:"<<b<<endl;
+}
