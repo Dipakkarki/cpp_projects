d2fddbc4c878c8367c98c4640d02e718df5e5db2
Dipakkarki <anubhabkarki66@gmail.com>
2021-07-02 23:10:42 +0545

    Operator overloading ++

---
+++ Assignment/assignment-4/unary.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class unary
+{
+    private:
+        int a,b,c;
+    public:
+        void getdata(int x,int y,int z)
+        {
+            a=x;
+            b=y;
+            c=z;
+        }
+        //return type operator op() ++ 
+        void operator ++()
+        {
+            a=a+2;
+            b=b+2;
+            c=c+2;
+        }
+        void display()
+        {
+            cout<<"Value of a:"<<a<<endl;
+            cout<<"Value of b:"<<b<<endl;
+            cout<<"Value of c:"<<c<<endl;
+        }
+};
+
+int main()
+{
+    unary obj1;
+    obj1.getdata(3,4,5);
+    cout<<"Before operator overloading:"<<endl;
+    obj1.display();
+    ++obj1;
+    cout<<"After operator overloading:"<<endl;
+    obj1.display();
+}
