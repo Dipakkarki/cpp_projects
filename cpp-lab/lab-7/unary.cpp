f452b19e5af6a7173eb59febbb7916e8b7a2d99f
Dipakkarki <anubhabkarki66@gmail.com>
2021-07-08 23:19:15 +0545

    using operator over;oad

--- cpp-lab/lab-7/unary.cpp
+++ cpp-lab/lab-7/unary.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class unary
-{
-    private:
-        int a,b,c;
+
+class unary {
+    int a, b, c;
+
     public:
-        void getdata(int x,int y,int z)
-            {       
-                a = x;
-                b = y;
-                c = z;
-            }
-//return type operator op()  --
-        void operator -()
-        {
-            a = -a;
-            b = -b;
-            c = -c;
-        }
-        void display()
-        {
-           cout<<"Value of a:"<<a<<endl;
-            cout<<"Value of b:"<<b<<endl;
-            cout<<"Value of c:"<<c<<endl; 
-    	}
+        void getdata(int X, int Y, int Z);
+        void operator -();
+        void display();
 };
 
-int main()
-{
-    unary u1;
-    u1.getdata(3,4,-5);
-    cout<<"Befor operator overloading:"<<endl;
-    u1.display();
-    -u1;
-    cout<<"After operator overloading:"<<endl;
-    u1.dispaly();
+void unary::getdata(int X, int Y, int Z) {
+    a = X;
+    b = Y;
+    c = Z;
 }
 
+void unary::operator -() {
+    a = -a;
+    b = -b;
+    c = -c;
+}
+
+void unary:: display() {
+    cout<<"Value of a:"<<a<<endl;
+    cout<<"Value of b:"<<b<<endl;
+    cout<<"Value of c:"<<c<<endl;
+}
+
+int main() {
+    unary obj;
+    obj.getdata(3,4,-5);
+    obj.display();
+
+    // calling overloaded operator
+    - obj;
+    obj.display();
+}
